bcb0a29b7a30df8a25dbc31fbaa0f3985ba1aacf
Colin Wheeler <cocoasamurai@gmail.com>
2020-10-08 21:19:19 -0700

    setup foundation of the network mock code

--- Sources/MockNetworking/MockURLProtocol.swift
+++ Sources/MockNetworking/MockURLProtocol.swift
@@ -13,3 +13,43 @@
 /// limitations under the License.
 
 import Foundation
+
+public final class MockURLProtocol: URLProtocol {
+	
+	//MARK: - Required URLProtocol API's
+	
+	private static var responses = [URL: HTTPURLResponse]()
+	
+	public override class func canInit(with request: URLRequest) -> Bool {
+		return true
+	}
+	
+	public override class func canInit(with task: URLSessionTask) -> Bool {
+		return true
+	}
+	
+	public override func startLoading() {
+		//
+	}
+	
+	public override func stopLoading() {
+		//
+	}
+	
+	//MARK: - Registration API's
+	
+	static var isRegistered = false
+	
+	public static func regigster(response: HTTPURLResponse, for url: URL) {
+		if !isRegistered {
+			URLProtocol.registerClass(MockURLProtocol.self)
+		}
+		responses[url] = response
+	}
+	
+	
+	
+	public static func unregister() {
+		URLProtocol.unregisterClass(MockURLProtocol.self)
+	}
+}

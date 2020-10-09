f30254a086bdd8b2e9cdbdbff126e1b176a35ec3
Colin Wheeler <cocoasamurai@gmail.com>
2020-10-08 21:35:15 -0700

    start setting up 1st test of mock response

--- Tests/MockNetworkingTests/MockNetworkingTests.swift
+++ Tests/MockNetworkingTests/MockNetworkingTests.swift
@@ -1,12 +1,19 @@
 import XCTest
-@testable import MockNetworking
+//@testable import MockNetworking
+import MockNetworking
 
 final class MockNetworkingTests: XCTestCase {
+	
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-		XCTAssertEqual(MockNetworking().major, 0)
+		let url = URL(string: "https://wwww.apple.com")!
+		let response = HTTPURLResponse(url: url,
+									   statusCode: 200,
+									   httpVersion: HTTPURLResponse.HTTP_1_1,
+									   headerFields: nil)!
+		MockURLProtocol.regigster(response: response, for: url)
     }
 
     static var allTests = [

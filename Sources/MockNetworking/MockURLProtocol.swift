6aeeb4e6c76fb8fa9001cad999b8aa8e12b49992
Colin Wheeler <cocoasamurai@gmail.com>
2020-10-18 15:27:07 -0700

    remove MockResponseStore from URLProtocol file

--- Sources/MockNetworking/MockURLProtocol.swift
+++ Sources/MockNetworking/MockURLProtocol.swift
@@ -48,37 +48,6 @@ public final class MockPropertyResponse {
 	}
 }
 
-fileprivate final class MockResponseStore {
-	fileprivate static let shared = MockResponseStore()
-	
-	private var _storedResponses = [URL: MockPropertyResponse]()
-	
-	init() {
-	}
-	
-	subscript(url: URL) -> MockPropertyResponse? {
-		get {
-			return _storedResponses[url]
-		}
-		
-		set {
-			_storedResponses[url] = newValue
-		}
-	}
-	
-	func contains(url: URL) -> Bool {
-		return _storedResponses.keys.contains(url)
-	}
-	
-	func removeAllReponses() {
-		_storedResponses.removeAll()
-	}
-	
-	func removeResponse(for url: URL) -> Bool {
-		return _storedResponses.removeValue(forKey: url) != nil
-	}
-}
-
 public final class MockURLProtocol: URLProtocol {
 	
 	public enum MockNetworkingErrors: Error {

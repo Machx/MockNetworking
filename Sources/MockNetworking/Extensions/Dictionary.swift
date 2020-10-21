873d7ccd72ac7f5c5f62a9faaae3095cf9cfe0f6
Colin Wheeler <cocoasamurai@gmail.com>
2020-10-20 20:04:43 -0700

    easy converting of header fields

--- Sources/MockNetworking/Extensions/Dictionary.swift
+++ Sources/MockNetworking/Extensions/Dictionary.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+extension Dictionary where Key == AnyHashable, Value == Any {
+	
+	/// HTTPURLProtocol initializer accepts headers as type [String:String] but
+	/// when you query for `allHeaderFields` you get back [AnyHashable:Any] this
+	/// is a convenience function for converting between the 2
+	
+	func getStringHeaders(from genericHeaders: [AnyHashable:Any]) -> [String:String] {
+		guard let convertedHeaders = genericHeaders as? [String:String] else { return [:] }
+		return convertedHeaders
+	}
+}

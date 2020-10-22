952a718565b0a151cbf4a9262b74ab15e8d7c824
Colin Wheeler <cocoasamurai@gmail.com>
2020-10-21 20:12:56 -0700

    move mock property response to its own file

--- Sources/MockNetworking/MockPropertyResponse.swift
+++ Sources/MockNetworking/MockPropertyResponse.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+public final class MockPropertyResponse {
+	typealias HTTPStatusCode = Int // Because Apple uses Int
+	
+	var response: HTTPURLResponse?
+	var body: Data?
+	var error: Error?
+	var httpVersion: String = HTTPURLResponse.HTTP_1_1
+	var bodyData: Data?
+	var delay: MockResponseDelay?
+	
+	init(url: URL,
+		 status: HTTPStatusCode = 200,
+		 httpVersion version: String = HTTPURLResponse.HTTP_1_1,
+		 headerFields headers: [String: String],
+		 body: Data? = nil,
+		 error requestError: Error? = nil,
+		 delay responseDelay: MockResponseDelay? = nil) {
+		
+		response = HTTPURLResponse(url: url,
+								   statusCode: status,
+								   httpVersion: version,
+								   headerFields: headers)
+		httpVersion = version
+		bodyData = body
+		error = requestError
+		delay = responseDelay
+	}
+	
+	var headers: [String:String]? {
+		guard let localHeaders = response?.allHeaderFields,
+			  let stringHeaders = localHeaders as? [String:String] else { return nil }
+		return stringHeaders
+	}
+	
+	var url: URL? {
+		return response?.url
+	}
+}
--- Sources/MockNetworking/MockURLProtocol.swift
+++ Sources/MockNetworking/MockURLProtocol.swift
@@ -19,41 +19,6 @@ public enum MockResponseDelay {
 	case range(ClosedRange<TimeInterval>)
 }
 
-public final class MockPropertyResponse {
-	typealias HTTPStatusCode = Int // Because Apple uses Int
-	
-	var response: HTTPURLResponse?
-	var body: Data?
-	var error: Error?
-	var httpVersion: String = HTTPURLResponse.HTTP_1_1
-	var bodyData: Data?
-	var delay: MockResponseDelay?
-	
-	init(url: URL,
-		 status: HTTPStatusCode = 200,
-		 httpVersion version: String = HTTPURLResponse.HTTP_1_1,
-		 headerFields headers: [String: String],
-		 body: Data? = nil,
-		 error requestError: Error? = nil,
-		 delay responseDelay: MockResponseDelay? = nil) {
-		
-		response = HTTPURLResponse(url: url,
-								   statusCode: status,
-								   httpVersion: version,
-								   headerFields: headers)
-		httpVersion = version
-		bodyData = body
-		error = requestError
-		delay = responseDelay
-	}
-	
-	var headers: [String:String]? {
-		guard let localHeaders = response?.allHeaderFields,
-			  let stringHeaders = localHeaders as? [String:String] else { return nil }
-		return stringHeaders
-	}
-}
-
 public final class MockURLProtocol: URLProtocol {
 	
 	public enum MockNetworkingErrors: Error {

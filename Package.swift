c5b14917014df2f3892b18c15cd560133be9a570
Colin Wheeler <cocoasamurai@gmail.com>
2021-04-26 22:59:06 -0700

    upate swift version and platforms

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -8,8 +8,6 @@ let package = Package(
 	platforms: [
 		.macOS(.v11),
 		.iOS(.v14),
-		.tvOS(.v13),
-		.watchOS(.v6)
 	],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.

731cbf2083948b8328a6301e327bfadad38649c2
Colin Wheeler <cocoasamurai@gmail.com>
2024-01-02 23:50:47 -0800

    revert back
    Signed-off-by: Colin Wheeler <cocoasamurai@gmail.com>

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "MockNetworking",
 	platforms: [
-		.macOS(.v14),
-		.iOS(.v17),
+		.macOS(.v13),
+		.iOS(.v16),
 	],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.

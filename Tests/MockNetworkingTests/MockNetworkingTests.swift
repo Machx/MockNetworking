eb4ea9802a6dd1b9e51c4551cfaff69d95af9189
Colin Wheeler <cocoasamurai@gmail.com>
2024-09-19 22:43:10 -0700

    move test out
    Signed-off-by: Colin Wheeler <cocoasamurai@gmail.com>

--- Tests/MockNetworkingTests/MockNetworkingTests.swift
+++ Tests/MockNetworkingTests/MockNetworkingTests.swift
@@ -138,6 +138,41 @@ func testRemoveResponse() throws {
 	#expect(MockURLProtocol.canInit(with: request) == false)
 }
 
+func testHeaders() throws {
+	let originalHeaders = [
+		"Thing1" : "Thing2"
+	]
+
+	let url = try XCTUnwrap(URL(string: "https://wwww.apple.com"))
+	let response = try XCTUnwrap(HTTPURLResponse(url: url,
+												 statusCode: 200,
+												 httpVersion: HTTPURLResponse.HTTP_1_1,
+												 headerFields: originalHeaders))
+
+	MockURLProtocol.register(response: response,
+							 for: url,
+							 withDelay: .range(1...2))
+	defer { MockURLProtocol.unregister() }
+
+	let expectation = XCTestExpectation()
+	var headers = [AnyHashable: Any]()
+	URLSession.sessionWith(.ephemeral).downloadTask(with: url) { (_, response, _) in
+		if let localResponse = response,
+		   let httpResponse = localResponse as? HTTPURLResponse {
+			headers = httpResponse.allHeaderFields
+		}
+		expectation.fulfill()
+	}.resume()
+
+	wait(for: [expectation], timeout: 2.0)
+
+	if let receivedHeaders = headers as? [String:String] {
+		XCTAssertEqual(receivedHeaders, originalHeaders)
+	} else {
+		XCTFail("not equatable")
+	}
+}
+
 final class MockNetworkingTests: XCTestCase {
 	
 	func testHeaders() throws {

a0316346331662b30ab4b72ced7fd1e6a6b76376
Colin Wheeler <cocoasamurai@gmail.com>
2024-09-22 15:18:28 -0700

    move tests in suite
    Signed-off-by: Colin Wheeler <cocoasamurai@gmail.com>

--- Tests/MockNetworkingTests/MockNetworkingTests.swift
+++ Tests/MockNetworkingTests/MockNetworkingTests.swift
@@ -23,231 +23,229 @@ enum MockNetworkingTestError: Error {
 
 @Suite("Mock Networking Tests")
 struct MockNetworkingTests {
-	
-}
+	@Test("Test Basic Mock Response")
+	func testBasicMockResponse() async throws {
+		guard let url = URL(string: "https://www.\(Int.random(in: 1...10000000)).com"),
+			  let response = HTTPURLResponse(url: url,
+											 statusCode: 200,
+											 httpVersion: HTTPURLResponse.HTTP_1_1,
+											 headerFields: nil) else { throw MockNetworkingTestError.couldNotUnwrapPreparedResponse }
 
-@Test("Test Basic Mock Response")
-func testBasicMockResponse() async throws {
-	guard let url = URL(string: "https://www.\(Int.random(in: 1...10000000)).com"),
-		  let response = HTTPURLResponse(url: url,
-										 statusCode: 200,
-										 httpVersion: HTTPURLResponse.HTTP_1_1,
-										 headerFields: nil) else { throw MockNetworkingTestError.couldNotUnwrapPreparedResponse }
+		MockURLProtocol.register(response: response, for: url)
+		defer { MockURLProtocol.unregister() }
 
-	MockURLProtocol.register(response: response, for: url)
-	defer { MockURLProtocol.unregister() }
+		var receivedURL: URL?
+		var receivedResponse: URLResponse?
+		var receivedError: Error?
 
-	var receivedURL: URL?
-	var receivedResponse: URLResponse?
-	var receivedError: Error?
+		do {
+			let (_,taskResponse) =  try await URLSession.sessionWith(.ephemeral).data(from: url)
+			receivedURL = response.url
+			receivedResponse = taskResponse
+		} catch {
+			receivedError = error
+		}
 
-	do {
-		let (_,taskResponse) =  try await URLSession.sessionWith(.ephemeral).data(from: url)
-		receivedURL = response.url
-		receivedResponse = taskResponse
-	} catch {
-		receivedError = error
+		#expect(url == receivedURL)
+		#expect(receivedResponse != nil)
+		#expect(receivedError == nil)
 	}
 
-	#expect(url == receivedURL)
-	#expect(receivedResponse != nil)
-	#expect(receivedError == nil)
-}
+	@Test("Test Basic Mock Response with Mock Type")
+	func testBasicMockResponseWithMockType() async throws {
+		guard let url = URL(string: "https://www.\(Int.random(in: 1...10000000)).com") else {
+			throw MockNetworkingTestError.couldNotUnwrapPreparedResponse
+		}
+		let mockResponse = MockPropertyResponse(url: url,
+												status: 200,
+												headerFields: [:])
 
-@Test("Test Basic Mock Response with Mock Type")
-func testBasicMockResponseWithMockType() async throws {
-	guard let url = URL(string: "https://www.\(Int.random(in: 1...10000000)).com") else {
-		throw MockNetworkingTestError.couldNotUnwrapPreparedResponse
-	}
-	let mockResponse = MockPropertyResponse(url: url,
-											status: 200,
-											headerFields: [:])
+		MockURLProtocol.registerMock(response: mockResponse, for: url)
+		defer { MockURLProtocol.unregister() }
 
-	MockURLProtocol.registerMock(response: mockResponse, for: url)
-	defer { MockURLProtocol.unregister() }
+		var receivedURL: URL?
+		var receivedResponse: URLResponse?
+		var receivedError: Error?
+		do {
+			let (_, taskResponse) = try await URLSession.sessionWith(.ephemeral).data(from: url)
+			receivedURL = taskResponse.url
+			receivedResponse = taskResponse
+		} catch {
+			receivedError = error
+		}
 
-	var receivedURL: URL?
-	var receivedResponse: URLResponse?
-	var receivedError: Error?
-	do {
-		let (_, taskResponse) = try await URLSession.sessionWith(.ephemeral).data(from: url)
-		receivedURL = taskResponse.url
-		receivedResponse = taskResponse
-	} catch {
-		receivedError = error
-	}
-
-	#expect(url == receivedURL)
-	#expect(receivedResponse != nil)
-	#expect(receivedError == nil)
-}
-
-@Test("Test Delay API")
-func testDelay() async throws {
-	guard let url = URL(string: "https://www.\(Int.random(in: 1...10000000)).com"),
-		  let response = HTTPURLResponse(url: url,
-										 statusCode: 200,
-										 httpVersion: HTTPURLResponse.HTTP_1_1,
-										 headerFields: nil) else {
-		throw MockNetworkingTestError.couldNotUnwrapPreparedResponse
+		#expect(url == receivedURL)
+		#expect(receivedResponse != nil)
+		#expect(receivedError == nil)
 	}
 
-	MockURLProtocol.register(response: response, for: url, withDelay: .time(1.0))
-	defer { MockURLProtocol.unregister() }
+	@Test("Test Delay API")
+	func testDelay() async throws {
+		guard let url = URL(string: "https://www.\(Int.random(in: 1...10000000)).com"),
+			  let response = HTTPURLResponse(url: url,
+											 statusCode: 200,
+											 httpVersion: HTTPURLResponse.HTTP_1_1,
+											 headerFields: nil) else {
+			throw MockNetworkingTestError.couldNotUnwrapPreparedResponse
+		}
 
-	let start = CFAbsoluteTimeGetCurrent()
-	let (_,_) = try await URLSession.sessionWith(.ephemeral).data(from: url)
-	let end = CFAbsoluteTimeGetCurrent()
+		MockURLProtocol.register(response: response, for: url, withDelay: .time(1.0))
+		defer { MockURLProtocol.unregister() }
 
-	let result = end - start
-	#expect(result >= 1.0)
-}
+		let start = CFAbsoluteTimeGetCurrent()
+		let (_,_) = try await URLSession.sessionWith(.ephemeral).data(from: url)
+		let end = CFAbsoluteTimeGetCurrent()
 
-@Test("Test Delay Range")
-func testDelayRange() async throws {
-	guard let url = URL(string: "https://www.\(Int.random(in: 1...10000000)).com"),
-		  let response = HTTPURLResponse(url: url,
-										 statusCode: 200,
-										 httpVersion: HTTPURLResponse.HTTP_1_1,
-										 headerFields: nil) else {
-		throw MockNetworkingTestError.couldNotUnwrapPreparedResponse
+		let result = end - start
+		#expect(result >= 1.0)
 	}
 
-	MockURLProtocol.register(response: response, for: url, withDelay: .range(1...2))
-	defer { MockURLProtocol.unregister() }
+	@Test("Test Delay Range")
+	func testDelayRange() async throws {
+		guard let url = URL(string: "https://www.\(Int.random(in: 1...10000000)).com"),
+			  let response = HTTPURLResponse(url: url,
+											 statusCode: 200,
+											 httpVersion: HTTPURLResponse.HTTP_1_1,
+											 headerFields: nil) else {
+			throw MockNetworkingTestError.couldNotUnwrapPreparedResponse
+		}
 
-	let start = CFAbsoluteTimeGetCurrent()
-	let (_,_) = try await URLSession.sessionWith(.ephemeral).data(from: url)
-	let end = CFAbsoluteTimeGetCurrent()
-	#expect((end - start) >= 1.0)
-}
+		MockURLProtocol.register(response: response, for: url, withDelay: .range(1...2))
+		defer { MockURLProtocol.unregister() }
 
-@Test("Test Remove Response")
-func testRemoveResponse() throws {
-	guard let url = URL(string: "https://www.\(Int.random(in: 1...10000000)).com"),
-		  let response = HTTPURLResponse(url: url,
-										 statusCode: 200,
-										 httpVersion: HTTPURLResponse.HTTP_1_1,
-										 headerFields: nil) else {
-		throw MockNetworkingTestError.couldNotUnwrapPreparedResponse
+		let start = CFAbsoluteTimeGetCurrent()
+		let (_,_) = try await URLSession.sessionWith(.ephemeral).data(from: url)
+		let end = CFAbsoluteTimeGetCurrent()
+		#expect((end - start) >= 1.0)
 	}
 
-	MockURLProtocol.register(response: response,
-							 for: url,
-							 withDelay: .range(1...2))
-	defer { MockURLProtocol.unregister() }
+	@Test("Test Remove Response")
+	func testRemoveResponse() throws {
+		guard let url = URL(string: "https://www.\(Int.random(in: 1...10000000)).com"),
+			  let response = HTTPURLResponse(url: url,
+											 statusCode: 200,
+											 httpVersion: HTTPURLResponse.HTTP_1_1,
+											 headerFields: nil) else {
+			throw MockNetworkingTestError.couldNotUnwrapPreparedResponse
+		}
 
-	let request = URLRequest(url: url)
-	#expect(MockURLProtocol.canInit(with: request) == true)
+		MockURLProtocol.register(response: response,
+								 for: url,
+								 withDelay: .range(1...2))
+		defer { MockURLProtocol.unregister() }
 
-	MockURLProtocol.clearResponse(for: url)
-	#expect(MockURLProtocol.canInit(with: request) == false)
-}
+		let request = URLRequest(url: url)
+		#expect(MockURLProtocol.canInit(with: request) == true)
 
-@Test("Test Headers")
-func testHeaders() async throws {
-	let originalHeaders = [
-		"Thing1" : "Thing2"
-	]
-
-	guard let url = URL(string: "https://www.\(Int.random(in: 1...10000000)).com"),
-		  let response = HTTPURLResponse(url: url,
-										 statusCode: 200,
-										 httpVersion: HTTPURLResponse.HTTP_1_1,
-										 headerFields: originalHeaders) else {
-		throw MockNetworkingTestError.couldNotUnwrapPreparedResponse
+		MockURLProtocol.clearResponse(for: url)
+		#expect(MockURLProtocol.canInit(with: request) == false)
 	}
 
-	MockURLProtocol.register(response: response,
-							 for: url,
-							 withDelay: .range(1...2))
-	defer { MockURLProtocol.unregister() }
+	@Test("Test Headers")
+	func testHeaders() async throws {
+		let originalHeaders = [
+			"Thing1" : "Thing2"
+		]
 
-	var headers = [AnyHashable: Any]()
-	let (_,taskResponse) = try await URLSession.sessionWith(.ephemeral).data(from: url)
-	if let httpResponse = taskResponse as? HTTPURLResponse {
-		headers = httpResponse.allHeaderFields
+		guard let url = URL(string: "https://www.\(Int.random(in: 1...10000000)).com"),
+			  let response = HTTPURLResponse(url: url,
+											 statusCode: 200,
+											 httpVersion: HTTPURLResponse.HTTP_1_1,
+											 headerFields: originalHeaders) else {
+			throw MockNetworkingTestError.couldNotUnwrapPreparedResponse
+		}
+
+		MockURLProtocol.register(response: response,
+								 for: url,
+								 withDelay: .range(1...2))
+		defer { MockURLProtocol.unregister() }
+
+		var headers = [AnyHashable: Any]()
+		let (_,taskResponse) = try await URLSession.sessionWith(.ephemeral).data(from: url)
+		if let httpResponse = taskResponse as? HTTPURLResponse {
+			headers = httpResponse.allHeaderFields
+		}
+
+		struct CouldNotCompareHeadersError: Error {}
+		guard let receivedHeaders = headers as? [String:String] else { throw CouldNotCompareHeadersError() }
+		#expect(receivedHeaders == originalHeaders)
 	}
 
-	struct CouldNotCompareHeadersError: Error {}
-	guard let receivedHeaders = headers as? [String:String] else { throw CouldNotCompareHeadersError() }
-	#expect(receivedHeaders == originalHeaders)
-}
+	@Test("Test Error Response")
+	func testErrorResponse() async throws {
+		guard let url = URL(string: "https://www.\(Int.random(in: 1...10000000)).com") else {
+			throw MockNetworkingTestError.couldNotUnwrapPreparedResponse
+		}
+		let error = NSError(domain: "com.MockNetworking.UnitTests",
+							code: 200,
+							userInfo: [ "Key1":"Value1"])
+		let mockResponse = MockPropertyResponse(url: url,
+												status: 200, httpVersion: HTTPURLResponse.HTTP_1_1,
+												headerFields: [:],
+												error: error)
 
-@Test("Test Error Response")
-func testErrorResponse() async throws {
-	guard let url = URL(string: "https://www.\(Int.random(in: 1...10000000)).com") else {
-		throw MockNetworkingTestError.couldNotUnwrapPreparedResponse
-	}
-	let error = NSError(domain: "com.MockNetworking.UnitTests",
-						code: 200,
-						userInfo: [ "Key1":"Value1"])
-	let mockResponse = MockPropertyResponse(url: url,
-											status: 200, httpVersion: HTTPURLResponse.HTTP_1_1,
-											headerFields: [:],
-											error: error)
+		MockURLProtocol.registerMock(response: mockResponse, for: url)
+		defer { MockURLProtocol.unregister() }
 
-	MockURLProtocol.registerMock(response: mockResponse, for: url)
-	defer { MockURLProtocol.unregister() }
+		var receivedError: NSError?
+		let request = URLRequest(url: url)
+		do {
+			let (_,_) = try await URLSession.sessionWith(.ephemeral).data(for: request)
+		} catch {
+			receivedError = error as NSError
+		}
 
-	var receivedError: NSError?
-	let request = URLRequest(url: url)
-	do {
-		let (_,_) = try await URLSession.sessionWith(.ephemeral).data(for: request)
-	} catch {
-		receivedError = error as NSError
-	}
-
-	#expect(receivedError?.code == error.code)
-	#expect(receivedError?.domain == error.domain)
-}
-
-@Test("Test Body Data")
-func testBodyData() async throws {
-	guard let url = URL(string: "https://www.\(Int.random(in: 1...10000000)).com") else {
-		throw MockNetworkingTestError.couldNotUnwrapPreparedResponse
+		#expect(receivedError?.code == error.code)
+		#expect(receivedError?.domain == error.domain)
 	}
 
-	let originalString = "Hello World"
-	let originalData = originalString.data(using: .utf8)
+	@Test("Test Body Data")
+	func testBodyData() async throws {
+		guard let url = URL(string: "https://www.\(Int.random(in: 1...10000000)).com") else {
+			throw MockNetworkingTestError.couldNotUnwrapPreparedResponse
+		}
 
-	let mockResponse = MockPropertyResponse(url: url,
-											status: 200,
-											headerFields: [:],
-											body: originalData)
+		let originalString = "Hello World"
+		let originalData = originalString.data(using: .utf8)
 
-	MockURLProtocol.registerMock(response: mockResponse, for: url)
-	defer { MockURLProtocol.unregister() }
+		let mockResponse = MockPropertyResponse(url: url,
+												status: 200,
+												headerFields: [:],
+												body: originalData)
 
-	let request = URLRequest(url: url)
-	let (data, _) = try await URLSession.sessionWith(.ephemeral).data(for: request)
-	#expect(data == originalData)
-}
+		MockURLProtocol.registerMock(response: mockResponse, for: url)
+		defer { MockURLProtocol.unregister() }
 
-@Test("Test HTTP Response")
-func testHTTPResponse() async throws {
-	guard let url = URL(string: "https://www.\(Int.random(in: 1...10000000)).com"),
-		  let response = HTTPURLResponse(url: url,
-										 statusCode: 200,
-										 httpVersion: HTTPURLResponse.HTTP_1_1,
-										 headerFields: ["thing180":"thing2"]) else {
-		throw MockNetworkingTestError.couldNotUnwrapVariable
+		let request = URLRequest(url: url)
+		let (data, _) = try await URLSession.sessionWith(.ephemeral).data(for: request)
+		#expect(data == originalData)
 	}
 
-	MockURLProtocol.register(response: response, for: url)
-	defer { MockURLProtocol.unregister() }
+	@Test("Test HTTP Response")
+	func testHTTPResponse() async throws {
+		guard let url = URL(string: "https://www.\(Int.random(in: 1...10000000)).com"),
+			  let response = HTTPURLResponse(url: url,
+											 statusCode: 200,
+											 httpVersion: HTTPURLResponse.HTTP_1_1,
+											 headerFields: ["thing180":"thing2"]) else {
+			throw MockNetworkingTestError.couldNotUnwrapVariable
+		}
 
-	let (_, receivedResponse) = try await URLSession.sessionWith(.ephemeral).data(from: url)
-	guard let receivedHTTPResponse = receivedResponse as? HTTPURLResponse else {
-		throw MockNetworkingTestError.couldNotUnwrapVariable
+		MockURLProtocol.register(response: response, for: url)
+		defer { MockURLProtocol.unregister() }
+
+		let (_, receivedResponse) = try await URLSession.sessionWith(.ephemeral).data(from: url)
+		guard let receivedHTTPResponse = receivedResponse as? HTTPURLResponse else {
+			throw MockNetworkingTestError.couldNotUnwrapVariable
+		}
+		// We can't compare the HTTPURLResponse we get back directly with an
+		// XCTAssertEqual(response,otherResponse) because it considers the text/mime type
+		// in equality, which ours will be nil and the received will be "text/plain"
+		#expect(response.url == receivedHTTPResponse.url)
+		#expect(response.statusCode == receivedHTTPResponse.statusCode)
+		#expect(response.allStringHTTPHeaders() == receivedHTTPResponse.allStringHTTPHeaders())
+
+		let header = receivedHTTPResponse.allHeaderFields["thing180"] as? String
+		#expect(header == "thing2")
 	}
-	// We can't compare the HTTPURLResponse we get back directly with an
-	// XCTAssertEqual(response,otherResponse) because it considers the text/mime type
-	// in equality, which ours will be nil and the received will be "text/plain"
-	#expect(response.url == receivedHTTPResponse.url)
-	#expect(response.statusCode == receivedHTTPResponse.statusCode)
-	#expect(response.allStringHTTPHeaders() == receivedHTTPResponse.allStringHTTPHeaders())
-
-	let header = receivedHTTPResponse.allHeaderFields["thing180"] as? String
-	#expect(header == "thing2")
 }
